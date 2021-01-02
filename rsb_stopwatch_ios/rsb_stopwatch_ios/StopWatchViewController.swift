9b9b666447c467a6c3fa452865ac61ea6ea4632f
Roman Brazhnikov <cold.steel.br@gmail.com>
2021-01-02 22:57:10 +0700

    Add StopWatchViewController

---
+++ rsb_stopwatch_ios/rsb_stopwatch_ios/StopWatchViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StopWatchViewController.swift
+//  rsb_stopwatch_ios
+//
+//  Created by Roman Brazhnikov on 02.01.2021.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+    
+}
