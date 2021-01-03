4d47090eb75febedcecc49ed2c26f5ced861a7c8
Roman Brazhnikov <cold.steel.br@gmail.com>
2021-01-03 11:06:20 +0700

    Launch screen, Date formatters & initial display on start

--- rsb_stopwatch_ios/rsb_stopwatch_ios/StopWatchViewController.swift
+++ rsb_stopwatch_ios/rsb_stopwatch_ios/StopWatchViewController.swift
@@ -9,4 +9,44 @@ import UIKit
 
 class StopWatchViewController: UIViewController {
     
+    // MARK: - Constants
+    private let shortFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "mm:ss.SS"
+        return df
+    }()
+    
+    private let longFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "HH:mm:ss.SS"
+        return df
+    }()
+    
+    // MARK: - Private properties
+    private var startTime: Double = .zero
+    private var currentValue: Double = .zero
+    
+    // MARK: - Outlets
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let components = DateComponents()
+        guard let date = Calendar.current.date(from: components) else { return }
+        let initialDisplay = shortFormatter.string(from: date)
+        displayLabel.text = initialDisplay
+    }
+    
+    // MARK: - Actions
+    @IBAction func leftButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rightButtonTapped(_ sender: UIButton) {
+    }
+    
+    // MARK: - Logic Private
+    
 }
